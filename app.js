e6bf7bc8d682f04d5c28f48dee3a86720e71b1f4
Jack Patterson <jackpatterson@jacks-mbp.lan>
2021-06-06 16:52:40 -0400

    need to fix chrome compatability

--- app.js
+++ app.js
@@ -14,6 +14,16 @@ app.set("view engine", "ejs");
 app.use(routes);
 app.use(express.static(__dirname + '/public'));
 
+app.use(function(req, res, next){
+    res.status(404);
+  
+    res.format({
+      html: function () {
+        res.render('404', { url: req.url })
+      },
+    })
+  });
+
 app.listen(app.get("port"), function () {
     console.log("Server has started on port " + app.get("port"))
 })
